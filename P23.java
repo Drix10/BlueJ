134724766853a34998042980cd9a8b429d71c505
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:09:03 +0530

    Add files via upload

---
+++ P23.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class P23
+{
+public static void main()
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter age");
+int a = sc.nextInt();
+if(a>=25)
+System.out.println("Eligible for voting");
+else
+System.out.println("Not eligible for voting");
+}
+} 
