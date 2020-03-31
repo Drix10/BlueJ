95e6ebd4c88bb520370c3e7f1ad8f4165c4f11b6
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:08:29 +0530

    Add files via upload

---
+++ P21.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P21
+{
+    
+public static void main()
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the numbers");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int s = a-(-b);
+    System.out.println("Sum of two numbers"+s);
+}
+}
