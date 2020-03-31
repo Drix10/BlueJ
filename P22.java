92258a510fd7e896fec692874ddfe38c2ed6279b
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:08:49 +0530

    Add files via upload

---
+++ P22.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class P22
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age");
+        int a = sc.nextInt();
+        if(a>=18)
+        System.out.println("Eligible for driving licence");
+        else;
+        System.out.println("Not eligible for driving licence");
+    }
+}
