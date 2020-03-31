0f6ed4d851441ae7c801f7dbeb4f585a5f409e4d
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 16:41:58 +0530

    Add files via upload

---
+++ P14.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class P14
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        
+      
+        int c = a*b;
+        System.out.println("Product of two numbers="+c);
+    }
+}
