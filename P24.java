5cfffbb7488654fac01138e86bec89f061cfbe43
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:09:24 +0530

    Add files via upload

---
+++ P24.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class P24
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a>b)
+        System.out.println("The greater number is "+a);
+        else
+        System.out.println("The greater number is "+b);
+    }
+}
+        
