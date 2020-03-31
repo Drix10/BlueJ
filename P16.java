7ce046ebd2a8de17a559d80f81368dd4c7b4d55e
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:02:46 +0530

    Add files via upload

---
+++ P16.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class P16
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of the cuboid");
+        int a = sc.nextInt();
+        System.out.println("Enter the width of the cuboid");
+        int b = sc.nextInt();
+        System.out.println("Enter the height of the cuboid");
+        int c = sc.nextInt();
+        int s = a*b*c;
+        System.out.println("Volume of a cuboid="+s);
+    }
+}
+        
