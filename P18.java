b2859b00c7445f46e07e9a987de15f8a96e28fe6
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 17:07:36 +0530

    Add files via upload

---
+++ P18.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class P18
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side");
+        int s = sc.nextInt();
+        int a = s*s;
+        System.out.println("Area of the square="+a);
+        int p = 4*s;
+        System.out.println("Perimeter of the square="+p);
+    }
+}
