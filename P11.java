53ec001de0132bf7d3c5984f0750dc448bb0f7e8
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 16:39:50 +0530

    Add files via upload

---
+++ P11.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class P11
+{
+    public static void main ()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int s = a+b;
+        System.out.println("Sum of two numbers:"+s);
+    }}
+        
