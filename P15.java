7e6481d0364373154b9259a94ab6ba34175ea967
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 16:42:55 +0530

    Add files via upload

---
+++ P15.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+public class P15
+{
+    public static void main()
+    {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the side of the square:");
+    int a = sc.nextInt();
+    int b = 4;
+    int s=a*b;
+    System.out.println("Perimeter of the square ="+s);
+}
+} 
+        
