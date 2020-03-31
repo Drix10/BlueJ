21d20fd73321977e05cd0c2e9edc4acc729876dc
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-03-31 16:40:58 +0530

    Add files via upload

---
+++ P12.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class P12
+{
+    public static void main ()
+    {
+        int num;
+        System.out.println("Enter an integer number:");
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+        if ( num % 2 == 0)
+        System.out.println("Entered number is even");
+        else
+        System.out.println("Entered number is odd");
+    }
+}
+        
+        
+        
