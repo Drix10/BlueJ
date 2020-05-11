738b0a2d1a25ba031c220788f132c1f9a810a446
Drishtant Ghosh <62929688+Drix10@users.noreply.github.com>
2020-05-11 12:52:36 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Q1
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter tempreture in farheniet");
+        int f = sc.nextInt(); 
+        int c = (5*(f-32)/9);
+        System.out.println("Temperature in centigrade ="+c);
+    }
+}
+        
+        
+    
+    
