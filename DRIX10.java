f77169d855aa5d74b13371f7da0773186b7770ff
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-05-15 10:54:39 +0530

    Add files via upload

---
+++ DRIX10.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class DRIX10
+{
+     public static void main()
+     {
+         Scanner sc=new Scanner(System.in);
+         System.out.print("D");System.out.print("D        ");System.out.print("R");System.out.print(" R   ");System.out.print("I");System.out.print("I");System.out.print("I");System.out.print("I");System.out.print("I ");System.out.print("X      ");System.out.println("     X");                                                  
+         System.out.print("D");System.out.print("  D      ");System.out.print("R");System.out.print("    R");System.out.print("  I");System.out.print("    X     ");System.out.println("   X");
+         System.out.print("D");System.out.print("    D    ");System.out.print("R");System.out.print("    R");System.out.print("  I");System.out.print("      X");System.out.println("    X");
+         System.out.print("D");System.out.print("     D   ");System.out.print("R");System.out.print(" R   ");System.out.print("  I");System.out.print("       X");System.out.println("  X");
+         System.out.print("D");System.out.print("     D   ");System.out.print("R");System.out.print("R    ");System.out.print("  I");System.out.print("       X");System.out.println("  X");
+         System.out.print("D");System.out.print("    D    ");System.out.print("R");System.out.print(" R   ");System.out.print("  I");System.out.print("      X");System.out.println("    X");
+         System.out.print("D");System.out.print("  D      ");System.out.print("R");System.out.print("  R  ");System.out.print("  I");System.out.print("    X");System.out.println("        X");
+         System.out.print("D");System.out.print("D        ");System.out.print("R");System.out.print("   R ");System.out.print("I");System.out.print("I");System.out.print("I");System.out.print("I");System.out.print("I ");System.out.print("X");System.out.println("          X");
+        }
+    }
