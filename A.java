717be89080f172ce36c74430b9a84993eb76b270
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-05-17 19:56:38 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class A
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+                              
+        System.out.print("         $ ");System.out.println(" $");
+        System.out.print("        $  ");System.out.println("  $");
+        System.out.print("       $   ");System.out.println("   $");
+        System.out.print("      $    ");System.out.println("    $");
+        System.out.print("     $  $ $");System.out.println("$ $  $ ");
+        System.out.print("    $      ");System.out.println("      $");
+        System.out.print("   $       ");System.out.println("       $");
+        System.out.print("  $        ");System.out.println("        $");
+        System.out.print(" $         ");System.out.println("         $");
+    }
+}
