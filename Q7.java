4fb9f6f8d621ff85fac80b01705048aafbfe229e
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-05-23 21:08:48 +0530

    Add files via upload

---
+++ Q7.java
@@ -0,0 +1,12 @@
+
+public class Q7
+{
+    public static void main()
+    {
+        double a=-6.35;
+        double b=14.74;
+        double x=Math.abs(Math.ceil(a));
+        double y=Math.round(Math.max(a,b));
+        System.out.println("The absolute value is "+x+"The roundoff value is"+y);
+    }
+}
