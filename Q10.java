674de094cabba294c6e5989acbce039844cf894c
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-06-04 09:31:04 +0530

    Add files via upload

---
+++ Q10.java
@@ -0,0 +1,15 @@
+public class Q10
+{
+    public static void main()
+    {
+      double a=-7;
+      double b=-2;
+      double c=-9;
+      double d=32.17;
+      double e=-6.7;
+      double x=Math.max(a,Math.min(b,c));
+      double y=Math.ceil(d);
+      double z=Math.abs(e);
+      System.out.println("the greatest num is"+x+"the roundof is"+y+"the absolute value is"+z);
+    }
+}
