b585432b25a79ae0716b379c8edc96d228635748
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-06-04 09:40:09 +0530

    Add files via upload

---
+++ Q15.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Q15
+{
+    public static void main()
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        a=sc.nextInt();
+        if(a>0)
+        System.out.println("The number is positive");
+        else if(a<0)
+        System.out.println("the number is negetive");
+        else
+        System.out.println("The number is not negetive or positive");
+    }}
+        
