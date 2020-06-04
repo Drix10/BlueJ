b6fe717e3e15fb27803b00cf6a2c75a47cc030e6
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-06-04 09:31:54 +0530

    Add files via upload

---
+++ Q11.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class Q11
+{
+    public static void main()
+    {
+        System.out.println(Math.sqrt(-9));
+        System.out.println(Math.round(4.5));
+        System.out.println(Math.round(-3.5));
+        System.out.println(Math.min(Math.ceil(32.7),Math.floor(2.9)));
+    }
+}
