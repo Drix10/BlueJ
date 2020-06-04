904ed59e24e4082b039b5e5552d9750177f0e0de
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-06-04 09:32:09 +0530

    Add files via upload

---
+++ Q12.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Q12
+{
+    public static void main()
+    {
+        double a=(Math.pow(25,3))+(Math.ceil(6.2));
+        double b=(Math.round(17.7))+(Math.floor(9));
+        double c=(Math.sqrt(Math.ceil(Math.abs(-48.1))));
+        double d=(Math.floor(8.1))+(Math.ceil(-7.3));
+        System.out.println("The number is "+a);
+        System.out.println("The number is "+b);
+        System.out.println("The number is "+c);
+        System.out.println("The number is "+d);
+    }
+}
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
