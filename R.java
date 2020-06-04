e74fbf609986810cbac9f61af6a8d461e1ffa791
Drishtant Ghosh <ggdrishtant@gmail.com>
2020-06-04 09:39:54 +0530

    Add files via upload

---
+++ R.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class R
+{
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("R");System.out.println("R");
+        System.out.print("R");System.out.println("  R");
+        System.out.print("R");System.out.println("  R");
+        System.out.print("R");System.out.println("R");
+        System.out.print("R");System.out.println("R");
+        System.out.print("R");System.out.println(" R");
+        System.out.print("R");System.out.println("  R");
+        System.out.print("R");System.out.println("   R");
+    }
+}
